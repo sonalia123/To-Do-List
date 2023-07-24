ef1105e5958cd9f2d69a2269e1b15838c848b054
sonalia123 <sonalikunwar50@gmail.com>
2023-07-24 10:19:43 +0530

    My To-Do List

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {TodoWrapper} from './Component/TodoWrapper';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <TodoWrapper />
+    </div>
+  );
+};
+
+export default App;
